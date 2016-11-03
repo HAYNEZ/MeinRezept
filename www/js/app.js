a6687c70f952b64bafc12de362892f6d689f54b5
VeronikaBertels <Veve.Bertels@web.de>
2016-11-03 21:21:47 +0100

    Tabs example

--- www/js/app.js
+++ www/js/app.js
@@ -17,48 +17,92 @@ angular.module('starter', ['ionic','ngCordova'])
       url: "/home",
       views: {
         'home-tab': {
-          templateUrl: "templates/main.html",
+          templateUrl: "templates/home.html",
           controller: 'HomeTabCtrl'
         }
       }
     })
-  });
+    .state('tabs.facts', {
+      url: "/facts",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts.html"
+        }
+      }
+    })
+    .state('tabs.facts2', {
+      url: "/facts2",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts2.html"
+        }
+      }
+    })
+    .state('tabs.about', {
+      url: "/about",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/about.html"
+        }
+      }
+    })
+    .state('tabs.navstack', {
+      url: "/navstack",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/nav-stack.html"
+        }
+      }
+    })
+    .state('tabs.contact', {
+      url: "/contact",
+      views: {
+        'contact-tab': {
+          templateUrl: "templates/contact.html"
+        }
+      }
+    });
+   $urlRouterProvider.otherwise("/tab/home");
+})
 
-  .controller('HomeTabCtrl', function($scope) {
+.controller('HomeTabCtrl', function($scope) {
   console.log('HomeTabCtrl');
 });
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-.controller('ListController' , function($scope) {
-  $scope.recipes = [
-    {title: 'Pancakes', image:"img/Pancakes_Example.png"},
-    {title: 'Fried chicken with cheese'},
-    {title: 'Thaicurry'},
-    {title: 'Thaicurry1'},
-    {title: 'Thaicurry2'},
-    {title: 'Thaicurry3'},
-    {title: 'Thaicurry4'}
+// .run(function($ionicPlatform) {
+//   $ionicPlatform.ready(function() {
+//     if(window.cordova && window.cordova.plugins.Keyboard) {
+//       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//       // for form inputs)
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//
+//       // Don't remove this line unless you know what you are doing. It stops the viewport
+//       // from snapping when text inputs are focused. Ionic handles this internally for
+//       // a much nicer keyboard experience.
+//       cordova.plugins.Keyboard.disableScroll(true);
+//     }
+//     if(window.StatusBar) {
+//       StatusBar.styleDefault();
+//     }
+//   });
+// })
 
-  ];
-.config(['$ionicConfigProvider', function($ionicConfigProvider) {
-
-    $ionicConfigProvider.tabs.position('bottom'); // other values: top
-
-}]
-})
+// .controller('MainControlelr', function($scope) {
+//
+// })
+// .controller('ListController' , function($scope) {
+//   $scope.recipes = [
+//     {title: 'Pancakes', image:"img/Pancakes_Example.png"},
+//     {title: 'Fried chicken with cheese'},
+//     {title: 'Thaicurry'},
+//     {title: 'Thaicurry1'},
+//     {title: 'Thaicurry2'},
+//     {title: 'Thaicurry3'},
+//     {title: 'Thaicurry4'}
+//
+//   ];
+// .config(['$ionicConfigProvider', function($ionicConfigProvider) {
+//     $ionicConfigProvider.tabs.position('bottom'); // other values: top
+// }]
+// })
